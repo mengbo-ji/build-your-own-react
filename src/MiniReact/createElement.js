d30b179bd556633d05174f12ba1abd28d7223087
mengbo-ji <jmb33595@163.com>
2021-10-18 23:03:03 +0800

    feat: mini react

---
+++ src/MiniReact/createElement.js
@@ -0,0 +1,25 @@
+/**
+ * 创建虚拟dom
+ * @param
+ * @type { import('react') } 
+*/
+function createElement(type, props, ...children) {
+  const childElements = [...children].reduce((result, child) => {
+    if (child !== false && child !== true && child !== null) {
+      if (child instanceof Object) {
+        result.push(child)
+      } else {
+        // 处理文本
+        result.push(createElement('text', { textContent: child }))
+      }
+    }
+    return result
+  }, [])
+  return {
+    type,
+    props: Object.assign({ children: childElements }, props),
+    children: childElements
+  }
+}
+
+export default createElement
