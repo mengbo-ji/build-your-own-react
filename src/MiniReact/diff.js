ddf2dd1cfa6537d8f49cc364e9931ef0b6cb43c6
mengbo-ji <jmb33595@163.com>
2021-10-27 22:22:32 +0800

    feat: 节点类型不同

--- src/MiniReact/diff.js
+++ src/MiniReact/diff.js
@@ -1,25 +1,28 @@
 import mountElement from './mountElement';
 import updateTextNode from './updateTextNode';
 import updateNodeElement from './updateNodeElement';
+import createDomElement from './createDomElement';
 
-function diff(virtualDom, root, oldDom) {
+function diff (virtualDom, root, oldDom) {
   const oldVirtaulDom = oldDom && oldDom._virtualDom
   if (!oldDom) {
     mountElement(virtualDom, root)
-  } else {
-    if(oldVirtaulDom && virtualDom.type === oldVirtaulDom.type) {
-      if (virtualDom.type === 'text') {
-        // 更新内容
-        updateTextNode(virtualDom, oldVirtaulDom, oldDom)
-      } else {
-        // 更新元素属性
-        updateNodeElement(oldDom, virtualDom, oldVirtaulDom)
-      }
+  } else if (virtualDom.type !== oldVirtaulDom.type && typeof virtualDom.type !== 'function') {
+    const newElement = createDomElement(virtualDom)
+    oldDom.parentNode.replaceChild(newElement, oldDom)
 
-      virtualDom.children.forEach((child, idx) => {
-        diff(child, oldDom, oldDom.childNodes[idx])
-      })
+  } else if (oldVirtaulDom && virtualDom.type === oldVirtaulDom.type) {
+    if (virtualDom.type === 'text') {
+      // 更新内容
+      updateTextNode(virtualDom, oldVirtaulDom, oldDom)
+    } else {
+      // 更新元素属性
+      updateNodeElement(oldDom, virtualDom, oldVirtaulDom)
     }
+
+    virtualDom.children.forEach((child, idx) => {
+      diff(child, oldDom, oldDom.childNodes[idx])
+    })
   }
 }
 
