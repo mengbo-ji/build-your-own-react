825957ceee6211749c78a936a5f8d2efcce0e4d7
mengbo-ji <jmb33595@163.com>
2021-10-28 22:35:27 +0800

    feat: setState 实现

--- src/MiniReact/Component.js
+++ src/MiniReact/Component.js
@@ -1,13 +1,18 @@
+import diff from "./diff"
+
 export default class Component {
   constructor(props){
     this.props = props
   }
   setState(state) {
     this.state = Object.assign({}, this.state, state)
+    // 获取新的虚拟dom
     const virtualDom = this.render()
-    const oldVirtualDom = this.getDom()
-    console.log('oldVirtualDom', oldVirtualDom)
-    console.log('virtualDom', virtualDom)
+    // 获取旧dom
+    const oldDom = this.getDom()
+    // 获取容器
+    const container = oldDom.parentNode
+    diff(virtualDom,container, oldDom)
   }
   setDom(dom) {
     this._dom = dom
